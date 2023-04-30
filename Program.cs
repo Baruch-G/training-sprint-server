82c1a6d746ca9dc88827b5e03a9f1b39ac97748a
Baruch Ginzburg <W050459990@gmail.com>
2023-04-30 20:29:42 +0300

    Cors origin policy issue fixed

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
         {
             policy
                 .WithOrigins(
-                    "http://localhost:5173"
+                    "http://localhost:5173", "http://192.168.1.15:5173"
                 )
                 .AllowAnyHeader()
                 .AllowAnyMethod()
